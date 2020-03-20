ac1045190bba71304c4d5afc2941f08ab5f4438e
Shirin Amouei <Shirin@Shirins-MacBook-Pro-4.local>
2020-03-20 19:59:54 -0400

    03.20.2020 update

--- daily_challenge.py
+++ daily_challenge.py
@@ -7,12 +7,28 @@ Created on Fri Mar 20 17:55:42 2020
 """
 
 
+from itertools import product 
 
 def combinator(lst, sep_=""):
 
-'''
-Given a list of string lists, returns a list of all 
-combinations as concatenated strings.
-'''
+    '''
+    Given a list of string lists, returns a list of all 
+    combinations as concatenated strings.
+    '''
 
     return [sep_.join(i) for i in product(*lst)]
+
+
+def is_prime(n):
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+def filter_primes(num):
+    '''
+    Takes a list and returns a new list containing only prime numbers.
+    '''
+    return [i for i in num if is_prime(i)]
+
+
